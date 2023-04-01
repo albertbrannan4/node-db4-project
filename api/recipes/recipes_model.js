0899dab7dce227a066436c89bf3f4b794f3d67d7
Albert Brannan <89121878+albertbrannan4@users.noreply.github.com>
2023-04-01 14:38:48 -0400

    routes made

--- api/recipes/recipes_model.js
+++ api/recipes/recipes_model.js
@@ -1,1 +1,13 @@
-// const db= require('./')
+async function getRecipes() {
+  const result = await Promise.resolve("getting all recipes");
+  return result;
+}
+
+async function getRecipe(id) {
+  const result = await Promise.resolve(`getting recipe ${id}`);
+  return result;
+}
+module.exports = {
+  getRecipes,
+  getRecipe,
+};
