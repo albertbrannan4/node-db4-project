1a4844f5fcc9a6e97bd2e591f597253cd487e975
Albert Brannan <89121878+albertbrannan4@users.noreply.github.com>
2023-04-01 15:48:35 -0400

    getting recipe names from the db

--- api/recipes/recipes_model.js
+++ api/recipes/recipes_model.js
@@ -1,13 +1,12 @@
 const db = require("../../data/db-config");
 
 async function getRecipes() {
-  //const result = await Promise.resolve("getting all recipes");
   const result = await db("recipes");
   return result;
 }
 
 async function getRecipe(id) {
-  const result = await Promise.resolve(`getting recipe ${id}`);
+  const [result] = await db("recipes").where("recipe_id", id);
   return result;
 }
 module.exports = {
